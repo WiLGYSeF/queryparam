0e100b0eb659e884388346d3a67f170a41f7bb68
WiLGYSeF <37422580+WiLGYSeF@users.noreply.github.com>
2021-05-20 03:11:30 -0400

    add basic template

--- queryparam.js
+++ queryparam.js
@@ -1,5 +1,45 @@
 class QueryParam {
-  constructor() {
+  constructor(url) {
     this.params = {};
-  }  
+
+    if (url === undefined) {
+      return;
+    }
+
+    const matches = url.matchAll(/[?&]+([^=&]+)(?:=([^&]*))?/g);
+    for (let i = 0; i < matches.length; i++) {
+      const m = matches[i];
+      if (m[1] in this.params && !this.unique) {
+        this.params[m[1]].push(m[2]);
+      } else {
+        this.params[m[1]] = [m[2]];
+      }
+    }
+  }
+
+  modify(mode, key, value) {
+    switch (mode) {
+      case '+': // add query parameter, override value if it exists
+        this.params[key] = [value];
+        break;
+      case '+^': // add query parameter to start even if it already exists
+        if (key in this.params) {
+          for (let i = this.params[key].length - 1; i !== 0; i--) {
+            this.params[key][i] = this.params[key][i + 1];
+          }
+        } else {
+          this.params[key] = [value];
+        }
+        break;
+      case '+$': // add query parameter to end even if it already exists
+        if (key in this.params) {
+          this.params[key].push(value);
+        } else {
+          this.params[key] = [value];
+        }
+        break;
+      default:
+        break;
+    }
+  }
 }
